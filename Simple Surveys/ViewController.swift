483142200b397b8a4d7596f3f8554da940492ca3
mguay22 <guay.me@gmail.com>
2020-02-24 15:19:46 -0500

    First commit

---
+++ Simple Surveys/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Simple Surveys
+//
+//  Created by Michael Guay on 2/24/20.
+//  Copyright © 2020 Michael Guay. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
